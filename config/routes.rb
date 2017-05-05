b6907000cc45863eb390910e9ad55d7ecb6bf7f5
Nacchamu <china.natsuki@gmail.com>
2017-05-05 20:38:54 +0900

    Make admin&user function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'notes#index'
   devise_for :users
-  resources :note, only:[:index, :show, :new]
-  resources :mypage, only:[:favorite, :pv] do
+  resources :notes, only:[:index, :show, :new, :create]
+  resources :mypages, only:[:favorite, :pv, :show] do
     get 'favorite'
     get 'pv'
   end
