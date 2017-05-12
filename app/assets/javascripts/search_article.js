095ac19b91d7177a7b545a7041d658b385b222e7
Nacchamu <china.natsuki@gmail.com>
2017-05-12 15:39:31 +0900

    Search article function

---
+++ app/assets/javascripts/search_article.js
@@ -0,0 +1,32 @@
+$(function() {
+  function buildHTML(data){
+    var thumbnail = $('<a href=""><img class="article__image" src="' + data.thumbnail.url + '">');
+    console.log(data.thumbnail);
+    var title = $('<div class="article__texts">').append('<a href=""><p class="article__texts__title">' + data.title);
+    var text = $(title).append('<div class="article__texts__detail">' + data.text).append('<p class="article__texts__date">' + data.created_at);
+    var article = $('<div class="article">').append(thumbnail).append(text);
+    $('.body__right').append(article);
+  }
+  $('.header__search__text').keypress(function(e){
+    var input = $('.header__search__text').val();
+    if ( e.which == 13 ) {
+      $.ajax({
+        type:'GET',
+        url: '/search',
+        data: {
+          title: input
+        },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        $('.body__right').empty();
+        $(data).each(function(index, ele){
+          buildHTML(ele);
+        })
+      })
+      .fail(function() {
+      })
+      return false;
+    }
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'notes#index'
+  get 'search' => 'notes#search'
   devise_for :users
   resources :notes, only: [:index, :show, :new, :create] do
     resources :favorites, only: [:create, :update, :destroy]
