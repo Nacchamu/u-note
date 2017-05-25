ac0330d62a708eb89261c1b07abb3312b2f85ee3
Nacchamu <china.natsuki@gmail.com>
2017-05-25 12:41:25 +0900

    Remove needless migrations

--- db/migrate/20170507112718_delete_articles_tag_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteArticlesTagTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :article_tags
-  end
-end
--- db/migrate/20170508065715_drop_article_categories_table.rb
+++
@@ -1,5 +0,0 @@
-class DropArticleCategoriesTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :article_categories
-  end
-end
