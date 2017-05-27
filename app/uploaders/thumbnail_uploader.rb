ec05d90d857a018478b568a23317661e47f38000
Nacchamu <china.natsuki@gmail.com>
2017-05-27 18:57:40 +0900

    Revise mistake

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -8,6 +8,9 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
