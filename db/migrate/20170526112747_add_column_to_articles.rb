f1a79dc4453bea2e5b3fcb5d5e8ce34807731e7f
Nacchamu <china.natsuki@gmail.com>
2017-05-27 14:10:54 +0900

    Revise mistake

--- db/migrate/20170526112747_add_column_to_articles.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToArticles < ActiveRecord::Migration[5.0]
-  def change
-    add_column :articles, :thumbnail, :text, :null => true
-    remove_column :articles, :articles, :integer
-  end
-end
